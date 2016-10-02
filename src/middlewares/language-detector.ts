a7e0499db057bc920774e9ac2582abcc729b0a8d
Guido García <guido.garciabernardo@telefonica.com>
2016-10-02 18:22:53 +0200

    [[CHORE]] More utterances and logs

--- src/middlewares/language-detector.ts
+++ src/middlewares/language-detector.ts
@@ -1,8 +1,10 @@
 import * as BotBuilder from 'botbuilder';
+import * as logger from 'logops';
 
 export default {
     botbuilder: (session: BotBuilder.Session, next: Function) => {
         // TODO
+        logger.info({textLocale: session.message.textLocale, textFormat: session.message.textFormat});
         next();
     }
 } as BotBuilder.IMiddlewareMap;
